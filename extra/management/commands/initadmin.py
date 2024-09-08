d3bc71b161f6f0f4edcbb2047d8c63a575dc441e
0xA <43623870+zylideum@users.noreply.github.com>
2024-09-08 14:01:22 -0700

    Refactored first-time password generation

--- extra/management/commands/initadmin.py
+++ extra/management/commands/initadmin.py
@@ -3,15 +3,22 @@ from django.core.management.base import BaseCommand
 # from django.conf import settings
 
 import os
+import string
+import secrets
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if User.objects.count() == 0:
             username = os.getenv("ADMIN_USERNAME")
             email = os.getenv("ADMIN_EMAIL")
-            password = User.objects.make_random_password()
+            password = self.generate_initial_password()
             print('Creating account for %s (%s)' % (username, email))
             admin = User.objects.create_superuser(email=email, username=username, password=password)
             admin.is_active = True
             admin.is_admin = True
             admin.save()
+    
+    # from https://docs.python.org/3/library/secrets.html#recipes-and-best-practices
+    def generate_initial_password(self):
+        alpha = string.ascii_letters + string.digits
+        password = ''.join(secrets.choice(alpha) for i in range(16))
